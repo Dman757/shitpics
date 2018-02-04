5b994a3bf47752645ca0e00b7f2b206b60d238ae
David Meade <dman757@gmail.com>
2018-02-03 20:41:09 -0800

    stupid css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">SHIT PICS</h1>
-        </header>
+      <div className='App'>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1 className='App-title'>SHIT PICS</h1>
         {/* <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        To get started, edit <code>src/App.js</code> and save to reload.
         </p> */}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
