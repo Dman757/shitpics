39906531dadc638e51d7670cb32a6e78eeeac42b
David Meade <dman757@gmail.com>
2018-06-26 20:01:41 -0700

    backup bumps

--- src/models/Shithouse.js
+++ src/models/Shithouse.js
@@ -6,8 +6,12 @@ import { getShit } from '../requester';
 const Shithouse = {}
 
 Shithouse.getAllBumps = async () => {
-  // const derp = await waitResponse(1000, bumps) //getShit()
   const ack = await getShit();
+  console.log('ack', ack.length)
+  if (ack.length <= 0) {
+    const derp = await waitResponse(10, bumps) //getShit()
+    return derp
+  }
   return ack
 }
 
